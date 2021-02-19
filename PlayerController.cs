59b87aea4c5bb45467343633995f5fff67de550e
Xmoure <79313865+xmoure-dot@users.noreply.github.com>
2021-02-19 14:00:16 +0200

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera came;
+
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            Ray ray = came.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) ;
+            { 
+                agent.SetDestination(hit.point);
+            }
+            
+        }
+    }
+}
+
+
